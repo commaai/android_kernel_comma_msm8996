eae4eb5bdc6303833900c0440c40d45747e88b45
Sultan Alsawaf <sultan@kerneltoast.com>
2018-12-06 22:10:24 -0800

    comma: Pass board name properly via device tree

--- drivers/comma/comma_board.c
+++ drivers/comma/comma_board.c
@@ -14,7 +14,7 @@
 #include <linux/bug.h>
 #include <linux/comma_board.h>
 #include <linux/kernel.h>
-#include <linux/string.h>
+#include <linux/of_fdt.h>
 
 static enum comma_id board_id;
 
@@ -25,16 +25,22 @@ enum comma_id comma_board_id(void)
 
 static int __init comma_board_init(void)
 {
-	const char *cmdline = saved_command_line;
+	const char *board_name;
+	unsigned long dt_root;
 
-	if (strstr(cmdline, "androidboot.baseband=apq"))
+	dt_root = of_get_flat_dt_root();
+	board_name = of_get_flat_dt_prop(dt_root, "comma,board-name", NULL);
+	if (!board_name) {
+		WARN(1, "Unknown Comma board! Please set comma,board-name");
+		return -ENODEV;
+	}
+
+	if (!strcmp(board_name, "cici"))
 		board_id = COMMA_BOARD_CICI;
-	else if (strstr(cmdline, "android.letv.product"))
+	else if (!strcmp(board_name, "leeco"))
 		board_id = COMMA_BOARD_LEECO;
-	else if (strstr(cmdline, "androidboot.project_name"))
+	else if (!strcmp(board_name, "oneplus"))
 		board_id = COMMA_BOARD_ONEPLUS;
-	else
-		WARN(1, "Unknown Comma board!");
 
 	return 0;
 }
