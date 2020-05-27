b97767d29da42be76f5ea752baf4f44fd521f637
Jason Young <jason@comma.ai>
2020-05-27 15:24:01 -0700

    Fix uninitialized variable

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -373,7 +373,7 @@ static int update_userspace_power(struct sched_params __user *argp)
 {
 	int i;
 	int ret;
-	int cpu;
+	int cpu = -1;
 	struct cpu_activity_info *node;
 	struct cpu_static_info *sp, *clear_sp;
 	int cpumask, cluster, mpidr;
@@ -396,7 +396,7 @@ static int update_userspace_power(struct sched_params __user *argp)
 		}
 	}
 
-	if (cpu >= num_possible_cpus())
+	if ((cpu < 0) || (cpu >= num_possible_cpus())
 		return -EINVAL;
 
 	node = &activity[cpu];
